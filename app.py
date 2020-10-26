651ac870bb6c0d3c99b7b6c5a6bc7328dfb18118
David Dvořák <dvorakdavid7@gmail.com>
2020-10-26 13:40:52 +0100

    env test

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return f"Hello World {os.getenv('TEST')}"
 
 
 if __name__ == '__main__':
